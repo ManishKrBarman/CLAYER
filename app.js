8f428886ae7e480685b808be92acb2f1ce4ca4e4
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2025-02-04 01:39:33 +0530

    changes

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ router.get("/", (req, res) => {
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server started on port 3000');
 })
